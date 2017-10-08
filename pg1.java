62fe9b33baf24fd7f996bae4ff8e89eb2400a298
Pavithra <31386825+pavi28@users.noreply.github.com>
2017-10-08 14:33:18 +0530

    Create pg1.java

---
+++ pg1.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+public class pg1
+{
+public static void main(String c[])
+{
+int a;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the number");
+a=sc.nextInt();
+if (a>0)
+{
+System.out.println("The entered number is positive");
+}
+else if(a==0)
+{
+System.out.println("The entered number is zero"); 
+}
+else
+{
+System.out.println("The entered number is negative");
+}
+}
