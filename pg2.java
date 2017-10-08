882954c5f3bb02ed370a6b1738cb5c453b67a1ed
Pavithra <31386825+pavi28@users.noreply.github.com>
2017-10-08 14:52:30 +0530

    Create pg2.java

---
+++ pg2.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+public class pg2
+{
+public static void main(String b[])
+{
+int a;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the number");
+a=sc.nextInt();
+if(a%2==0)
+{
+System.out.println("The entered number is even");
+}
+else
+{
+System.out.println("The entered number is odd");
+}
+}
+}
