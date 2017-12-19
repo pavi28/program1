4efb99f31f2264ae42ce72dd5d12348d7b9fa444
Pavithra <31386825+pavi28@users.noreply.github.com>
2017-12-19 20:53:38 +0530

    Create hello.java

---
+++ hello1.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class hello1
+{
+public static void main(String a[])
+{
+String n;
+Scanner b=new Scanner(System.in);
+System.out.println("enter the word hello");
+n=b.next();
+for(int i=0;i<=4;i++)
+System.out.println(n);
+}
+}
