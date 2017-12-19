5e83f4b03e988774add8472d47691c4436048adc
Pavithra <31386825+pavi28@users.noreply.github.com>
2017-12-19 21:12:59 +0530

    Create nn1.java

---
+++ nn1.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+class nn
+{
+public static void main(String a[])
+{
+int d=0;
+Scanner b=new Scanner(System.in);
+System.out.println("enter the number");
+int n=b.nextInt();
+for(int i=0;i<=n;i++)
+{
+ d=d+i;
+}
+System.out.print(d);
+}
+}
