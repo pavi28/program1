f61669548cc1a691ffcf8b85f2d2729f81dbf6e6
Pavithra <31386825+pavi28@users.noreply.github.com>
2017-12-25 12:19:06 +0530

    al.java

---
+++ al.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class al
+{
+public static void main(String a1[])
+{
+char b;
+Scanner c=new Scanner(System.in);
+System.out.println("Enter the character");
+b=c.next().charAt(0);
+if(b=='a'||b=='b'||b=='c'||b=='d'||b=='e'||b=='f'||b=='g'||b=='h'||b=='i'||b=='j'||b=='k'||b=='l'||b=='m'||b=='n'||b=='o'||b=='p'||b=='q'||b=='r'||b=='s'||b=='t'||b=='u'||b=='v'||b=='w'||b=='x'||b=='y'||b=='z'||b=='A'||b=='B'||b=='C'||b=='D'||b=='E'||b=='F'||b=='G'||b=='H'||b=='I'||b=='J'||b=='K'||b=='L'||b=='M'||b=='N'||b=='O'||b=='P'||b=='Q'||b=='R'||b=='S'||b=='T'||b=='U'||b=='V'||b=='W'||b=='X'||b=='Y'||b=='Z')
+System.out.println("Alphabet");
+else
+System.out.println("Not An Alphabet");
+}
+}
