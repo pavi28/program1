773fdf53250177a982658ea2a7b00da9878a120b
Pavithra <31386825+pavi28@users.noreply.github.com>
2017-12-25 12:28:41 +0530

    create ly.java

---
+++ ly.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class ly
+{
+public static void main(String a1[])
+{
+int b;
+Scanner c=new Scanner(System.in);
+System.out.println("Enter the character");
+b=c.nextInt();
+if((b%4)==0)
+System.out.println("Yes");
+else
+System.out.println("No");
+}
+}
