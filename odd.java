549c3a61bd70f7c2fc7ab6c86a07320670223fac
Pavithra <31386825+pavi28@users.noreply.github.com>
2018-01-05 22:20:47 +0530

    Create odd.java

---
+++ odd.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+class odd
+{
+public static void main(String a1[])
+{
+int a,c;
+Scanner b=new Scanner(System.in);
+System.out.println("Enter the first number");
+a=b.nextInt();
+System.out.println("Enter the second number");
+c=b.nextInt();
+System.out.println("THE EVEN NUMBERS BETWEEN THE INTERVALS ARE :");
+for(int i=a+1;i<c;i++)
+{
+if((i%2)!=0)
+{
+System.out.print(i+" ");
+}
+}
+}
+}
