a5975c1e0f5158bf3386a61bc12883bd27e26a19
Pavithra <31386825+pavi28@users.noreply.github.com>
2018-01-07 22:57:12 +0530

    mina.java

---
+++ mina.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+class mina
+{
+public static void main(String a1[])
+{
+int a,i,d=0;
+
+int d1[]=new int[30];
+Scanner c=new Scanner(System.in);
+System.out.print("enter the number of elements ");
+a=c.nextInt();
+System.out.println("enter the numbers");
+for(i=0;i<a;i++)
+d1[i]=c.nextInt();
+for(int j=0;j<a;j++)
+{
+if(d1[j]<d1[j+1])
+{
+d1[j+1]=d1[j];
+d=d1[j];
+}
+}
+System.out.println(d +" is the minimum element");
+}
+}
