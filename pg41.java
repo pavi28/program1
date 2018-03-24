9e6a0080845798e365175f0465ce35d1c1052859
Pavithra <31386825+pavi28@users.noreply.github.com>
2018-03-24 21:32:55 +0530

    create pg41.java

---
+++ pg41.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class pg41
+{
+public static void main(String a1[])
+{
+int a;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the number");
+a=sc.nextInt();
+for(int i=1;i<=a;i++)
+{
+System.out.println("laptop");
+}
+}
+}
