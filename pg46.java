00821e52535c03e1d589cc80ddbe673a04319448
Pavithra <31386825+pavi28@users.noreply.github.com>
2018-04-05 20:17:54 +0530

    pg46.java

---
+++ pg46.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class pg46
+{
+public static void main(String a1[])
+{
+int a;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the number");
+a=sc.nextInt();
+a=a+1;
+System.out.println(a);
+}
+}
