cfc41b9544e31bc9cc1edbe3a40110695b3aae1e
Pavithra <31386825+pavi28@users.noreply.github.com>
2018-06-26 04:51:16 +0000

    numrev.java

---
+++ numrev.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class numrev
+{
+public static void mian(String a[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int a1=0;
+while(n>0)
+{
+a1=n%10;
+System.out.print(a1);
+n=n/10;
+}
+}
