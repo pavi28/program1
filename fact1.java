591cfe74c1202bacae1bb9feea25c39f3aef8206
Pavithra <31386825+pavi28@users.noreply.github.com>
2018-06-26 04:38:44 +0000

    fact1.java

---
+++ fact1.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class fact1
+{
+public static void main(String a[])
+{
+Scanner sc=new Scanner(System.in);
+int h=sc.nextInt();
+fact=1;
+for(int i=1;i<h;i++)
+{
+fact=fact*i;
+}
+System.out.println(fact);
+}
+}
