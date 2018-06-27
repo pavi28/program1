d2fafae027a4d27d4b53f92d4ed4c9653f23f176
Pavithra <31386825+pavi28@users.noreply.github.com>
2018-06-27 04:05:10 +0000

    Period.java

---
+++ Period.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class Period
+{
+public static void main(String a[])
+{
+Scanner sc=new Scanner(System.in);
+String s=sc.next();
+String h=s+".";
+System.out.println(h);
+}
+}
