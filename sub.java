f390e7abe62d0903984a00f9824d2e4741c41b25
Pavithra <31386825+pavi28@users.noreply.github.com>
2018-07-08 14:43:48 +0530

    sub.java

---
+++ sub.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+public class sub
+{
+    public static void main(String a[])
+    {
+        String str=a[0];
+        String str1=a[1];
+        System.out.println(str.indexOf(str1));
+    }
+}
